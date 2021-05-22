dd885bc7b8b3ee5bafadb0777547e68c73627e0d
Jeremy Poulter <jpoulter@webgains.com>
2021-05-22 17:33:31 +0100

    Use the tesla_login helper to login to the Tesla API

--- src/view_models/TeslaViewModel.js
+++ src/view_models/TeslaViewModel.js
@@ -10,21 +10,52 @@ function TeslaViewModel(baseEndpoint, config)
   this.mfaPassCode = ko.observable("");
   this.mfaDeviceName = ko.observable("");
 
-  this.login = () => {
-    /*
-    tesla_login({
-      identity: this.username(),
-      credential: this.password(),
-      mfaPassCode: this.mfaPassCode(),
-      mfaDeviceName: this.mfaDeviceName()
-    }, (error, response, body) => {
-      console.log(error);
-      console.log(response);
-      console.log(body);
-      
-      
-    });
-    */
+  this.fetching = ko.observable(false);
+  this.success = ko.observable(false);
+
+  const teslaLogin = "http://localhost:3000/login";
+
+  this.setForTime = function (flag, time) {
+    flag(true);
+    setTimeout(() => { flag(false); }, time);
   };
 
+  this.login = () => {
+    this.fetching(true);
+    this.success(false);
+    $.ajax(teslaLogin, {
+      method: "POST",
+      data: JSON.stringify({
+        username: this.username(),
+        password: this.password(),
+        mfaPassCode: this.mfaPassCode(),
+        mfaDeviceName: this.mfaDeviceName()
+      }),
+      contentType: "application/json"
+    }).done((credentials) => {
+      $.ajax(baseEndpoint() + "/config", {
+        method: "POST",
+        data: JSON.stringify({
+          tesla_enabled: true,
+          tesla_access_token: credentials.access_token,
+          tesla_refresh_token: credentials.refresh_token,
+          tesla_created_at: credentials.created_at,
+          tesla_expires_in: credentials.expires_in
+        }),
+        contentType: "application/json"
+      }).done(() => {
+        config.tesla_enabled(true);
+        config.tesla_access_token(credentials.access_token);
+        config.tesla_refresh_token(credentials.refresh_token);
+        config.tesla_created_at(credentials.created_at);
+        config.tesla_expires_in(credentials.expires_in);
+        this.setForTime(this.success, 2000);
+      }).fail(() => {
+      }).always(() => {
+        this.fetching(false);
+      });
+    }).fail(() => {
+      this.fetching(false);
+    });
+  };
 }
